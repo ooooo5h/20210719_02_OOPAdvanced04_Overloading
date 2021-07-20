22641ce5e46a0f918c081f0848e69f8a55c05381
전은형 <ooooo5h91@gmail.com>
2021-07-20 22:52:32 +0900

    메쏘드이름이 같아도 파라미터값이 다르면 다른 메쏘드로 인식함 = 오버로딩

--- src/codes/MainDrive.java
+++ src/codes/MainDrive.java
@@ -1,5 +1,16 @@
 package codes;
 
 public class MainDrive {
+	
+	public static void main(String[] args) {
+		
+		MyTestClass mtc = new MyTestClass();
+		
+//		오버로딩이 중복이 아닌걸로 인정되는(괜찮은) 이유를 체험해보자
+		mtc.testMethod();
+		
+		mtc.testMethod(10);
+		
+	}
 
 }
