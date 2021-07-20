b889ff5e4bbb062465de6885010e91bd0200b751
전은형 <ooooo5h91@gmail.com>
2021-07-20 22:50:13 +0900

    메쏘드 두개 생성
    이름동일, 파라미터가 하나는 없고 하나는 int를 받을 때 다른 메쏘드로 인식한다.

---
+++ src/codes/MyTestClass.java
@@ -0,0 +1,13 @@
+package codes;
+
+public class MyTestClass {
+	
+	void testMethod() {
+		System.out.println("기본 테스트 메쏘드입니다.");
+	}
+	
+	void testMethod(int num) {
+		System.out.println("정수 하나를 입력받는 테스트 메쏘드입니다.");
+	}
+
+}
